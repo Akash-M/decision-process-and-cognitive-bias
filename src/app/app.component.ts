e99716da6f144c2fa311358395c7cc91ddeabf62
Akash Manjunath <akash.manjunath@gmail.com>
2018-01-23 21:22:07 +0100

    Router fix.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
 declare const $: any;
 
@@ -9,11 +10,23 @@ declare const $: any;
 })
 export class AppComponent implements OnInit {
 
-  constructor() {}
+  constructor(private router:Router) {}
 
   ngOnInit() {
       $.material.options.autofill = true;
       $.material.init();
+      this.router.routeReuseStrategy.shouldReuseRoute = function(){
+          return false;
+      };
+
+      this.router.events.subscribe((evt) => {
+          if (evt instanceof NavigationEnd) {
+              // trick the Router into believing it's last link wasn't previously loaded
+              this.router.navigated = false;
+              // if you need to scroll back to top, here is the right place
+              window.scrollTo(0, 0);
+          }
+      });
   }
 
 }
